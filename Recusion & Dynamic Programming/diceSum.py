89c0595b68a27a6497a837e9102b9961162d7534
Joey <s3589@hotmail.com>
2015-09-10 23:55:12 -0400

    update

--- Recusion & Dynamic Programming/diceSum.py
+++ Recusion & Dynamic Programming/diceSum.py
@@ -1,16 +1,65 @@
 '''given n, the number of dies, m the number of faces on each die, and s, the total the dies
-have to sum up to, return the number of die combinations there are (must use all dies)'''
+have to sum up to, return the number of die combinations there are (must use all dies)
 
-'''wrong right now, gives permutations'''
+n = dies
+m = faces
+s = sum
+'''
+
 def diceSum(n,m,s):
+	mem = {}
+	def helper(n,m,s):
+		if (n,m,s) in mem.keys():
+			return mem[(n,m,s)]
+		elif n*m < s or n > s:
+			return 0
+		elif n == 1 or n*m == s:
+			return 1
+		else:
+			count = 0
+			for i in range(s//m+1):
+				count += helper(n-i, m-1, s-m*i)
+			mem[(n,m,s)] = count
+			return count
+
+	return helper(n,m,s)
+
+print(diceSum(100,100,1000))
+
+''' Algorithm:
+given (4,4,10):
+
+f(4,4,10):
+
+	0*4 -> f(4,3,10):
+				0*3 -> f(4,2,10) = 0 	(basecase: n*m < s)
+				1*3 -> f(3,2,7) = 0		(basecase: n*m < s)
+				2*3	-> f(2,2,4) = 1 	(basecase: n*m = s)					3,3,2,2
+				3*3 -> f(1,2,1) = 1 	(basecase: n = 1)					3,3,3,1
+
+	1*4 -> f(3,3,6):
+				3*0 -> f(3,2,6) = 1 	(basecase: n*m = s)					4,2,2,2
+				3*1 -> f(2,2,3):
+							0*2 -> f(2,1,3) = 0		(basecase: n*m < s)
+							1*2 -> f(1,1,1) = 1 	(basecase: n = 1)		4,3,2,1
+
+				3*2	-> f(1,2,0) = 0		(basecase: n > s)
+
+	2*4 -> f(2,3,2):
+				0*3 -> f(2,2,2):
+							0*2 -> f(2,1,2) = 1 	(basecase: n*m = s)		4,4,1,1
+							1*2 -> f(1,1,0) = 0		(basecase: n > 0) 
+'''
+
+
+'''this is wrong. this gives permutations instead'''
+def diceSum1(n,m,s):
 	if n*m < s or n > s:
 		return 0
 	elif n == 1:
 		return 1
 	else:
-		subSum = 0
+		count = 0
 		for i in range(1,m+1):
-			subSum += diceSum(n-1,m,s-i)
-		return subSum
-
-print(diceSum(2,2,2))
+			count += count(n-1,m,s-i)
+		return count
--- Recusion & Dynamic Programming/longestCommonSubsequence.py
+++ Recusion & Dynamic Programming/longestCommonSubsequence.py
@@ -1,72 +1,32 @@
 '''given 2 strings, find the longest common subsequence:
 ie: abcde, xbcfge -> bce'''
 
-'''This returns just length'''
-def LCSLen(str1, str2):
-
-	res = {}
-	def helper(idx1,idx2):
-
-		if idx1 == -1 or idx2 == -1:
-			return 0
-
-		elif str1[idx1] == str2[idx2]:
-			if (idx1, idx2) in res.keys():
-				return res[(idx1, idx2)]				
-			else:
-				val = helper(idx1-1, idx2-1)
-				res[(idx1, idx2)] = 1 + val
-				return 1 + val
-		else:
-			if ((idx1-1),idx2) in res.keys():
-				val1= res[((idx1-1),idx2)]
-			else:
-				val1 = helper((idx1-1),idx2)
-				res[((idx1-1),idx2)] = val1
-
-
-			if (idx1,(idx2-1)) in res.keys():
-				val2 = res[(idx1,(idx2-1))]
-			else:
-				val2 = helper(idx1,(idx2-1))
-				res[(idx1,(idx2-1))] = val2
-
-			return max(val1,val2)
-
-	maxLen = helper(len(str1)-1,len(str2)-1)
-	return maxLen
-
 '''This returns the subsequence'''
 def LCS(str1, str2):
-	res = {}
+	mem = {}
 	def helper(idx1,idx2):
-		if idx1 == -1 or idx2 == -1:
+		if (idx1,idx2) in mem.keys():
+			return mem[(idx1,idx2)]
+
+		elif idx1 == -1 or idx2 == -1:
 			return ""
 
 		elif str1[idx1] == str2[idx2]:
-			if (idx1, idx2) in res.keys():
-				return res[(idx1, idx2)]
-			else:
-				s = helper(idx1-1,idx2-1)+str1[idx1]
-				res[(idx1, idx2)] = s
-				return s
-		else:
-			if (idx1-1,idx2) in res.keys():
-				s1 = res[(idx1-1,idx2)]
-			else:
-				s1 = helper(idx1-1,idx2)
-				res[(idx1-1,idx2)] = s1
+			s = helper(idx1-1,idx2-1)+str1[idx1]
+			res = s
 
-			if (idx1,idx2-1) in res.keys():
-				s2 = res[(idx1,idx2-1)]
-			else:
-				s2 = helper(idx1,idx2-1)
-				res[(idx1,idx2-1)] = s2
+		else:
+			s1 = helper(idx1-1,idx2)
+
+			s2 = helper(idx1,idx2-1)
 
 			if len(s1) > len(s2):
-				return s1
+				res = s1
 			else:
-				return s2
+				res = s2
+
+		mem[(idx1,idx2)] = res
+		return res
 
 	return helper(len(str1)-1,len(str2)-1)
 
