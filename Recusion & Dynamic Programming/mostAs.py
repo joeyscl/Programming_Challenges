626fbe5b00668297afd6699c671a6d5a55a9bddb
joeyscl <s3589@hotmail.com>
2015-09-24 14:39:15 -0400

    Update mostAs.py
    Implemented DP; much better performance

--- Recusion & Dynamic Programming/mostAs.py
+++ Recusion & Dynamic Programming/mostAs.py
@@ -2,8 +2,28 @@
 determine the maximum number of a's an user can output on the screen given N
 number of keystrokes'''
 
+''' O(n) '''
+def mostAs_DP(n):
+  As = [0,1,2,3,4,5] + [0]*(n-5)
+  copied = [1,1,1,1,1,1] + [0]*(n-5)
 
-def mostAs(num):
+  if n <= 5:
+    return As[n]
+  else:
+    for i in range(6,n+1):
+      if As[i-3]*2 >= As[i-1]+copied[i-1]:
+        As[i] = 2*As[i-3]
+        copied[i] = As[i-3]
+      else:
+        As[i] = As[i-1] + copied[i-1]
+        copied[i] = copied[i-1]
+        
+  # print(As)
+  # print(copied)        
+  return As[-1]
+
+''' Not as Good. ~O(N^2)? '''
+def mostAs_Rec(num):
 	mem = {}
 	def helper(num, printed, copied):
 		if (num, printed, copied) in mem.keys():
@@ -16,8 +36,9 @@ def mostAs(num):
 
 		elif num >= 3 and printed != 0:
 			#compare select-copy-paste whole thing vs paste existing clipboard
-			mem[(num, printed, copied)] = max(helper(num-3,printed*2,printed), helper(num-1,printed+copied,copied))
-			return mem[(num, printed, copied)]
+			res = max(helper(num-3,printed*2,printed), helper(num-1,printed+copied,copied))
+			mem[(num, printed, copied)] = res
+			return res
 
 		else: # printed == 0, copied == 0
 			best = 0
@@ -29,4 +50,4 @@ def mostAs(num):
 
 	return helper(num,0,0)
 
-print(mostAs(20))
+print(mostAs_Rec(20))
